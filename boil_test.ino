c64c1b31adf7ab82059e642e45a584339069af52
PaVel <vel.pavel@gmail.com>
2015-03-02 12:56:29 +0300

    First working version

---
+++ boil_test.ino
@@ -0,0 +1,71 @@
+#include <OneWire.h>
+
+#define BTN_PIN 3
+#define LED_PIN 13
+OneWire ds(10);
+unsigned long start_ms=0;
+boolean status_run=0;
+ 
+void setup() {
+  Serial.begin(38400);
+  randomSeed(analogRead(0)); 
+  pinMode(BTN_PIN, INPUT);
+  pinMode(LED_PIN, OUTPUT);
+}
+ 
+void loop() {
+  static long random_id=0;
+  static boolean boil=false;
+  if (digitalRead(BTN_PIN))
+  {
+    status_run = !status_run;
+    if (status_run)
+    {
+      boil = false;
+      start_ms = millis();
+      random_id=random(64000);
+      Serial.println("START");
+    }
+    else
+    {
+      digitalWrite(LED_PIN, LOW);
+      Serial.println("STOP");
+      delay(1000);
+    }
+  }
+  if (status_run)
+  {
+    int tempr=ReadTemp();
+    Serial.print("RID: ");
+    Serial.print(random_id);
+    Serial.print(" :MS: ");
+    Serial.print(millis()-start_ms);
+    Serial.print(" :T: ");
+    Serial.print(tempr);
+    if (tempr>=99 && !boil) {Serial.print(" :boil: 1"); boil= true;}
+    else if (boil && tempr<99) {Serial.print(" :boil: -1"); boil= false;}
+    else {Serial.print(" :boil: 0");}
+    Serial.println();
+    
+    //Светодиод
+    if (boil) {digitalWrite(LED_PIN, HIGH);}
+    else {digitalWrite(LED_PIN, !digitalRead(LED_PIN));}
+  }
+}
+
+int ReadTemp()
+{
+  byte data[2];
+  ds.reset(); 
+  ds.write(0xCC);
+  ds.write(0x44);
+  delay(750);
+  ds.reset();
+  ds.write(0xCC);
+  ds.write(0xBE);
+  data[0] = ds.read(); 
+  data[1] = ds.read();
+  int Temp = (data[1]<< 8)+data[0];
+  Temp = Temp>>4;
+  return Temp;
+}
---
+++ from_com.py
@@ -0,0 +1,54 @@
+﻿#-------------------------------------------------------------------------------
+# Name:        модуль1
+# Purpose:
+#
+# Author:      Pavel
+#
+# Created:     11.11.2014
+# Copyright:   (c) Pavel 2014
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+import serial
+import datetime
+serial_port = 'COM8'
+serial_speed = 38400
+
+def init_serial():
+    return serial.Serial(serial_port,serial_speed)
+
+def save_to_file():
+    ser=init_serial()
+    #f_out = open('temperature_'+datetime.datetime.now().strftime("%y_%m_%d_%H_%M_%S")+'.csv', 'w')
+    f_out_name='temperature.csv'
+#    f_out = open(f_out_name, 'a')
+    #ser=serial.Serial(serial_port,serial_speed)
+    print("Wait data")
+    while 1:
+        try:
+            s = ser.readline().decode('ASCII').strip()
+        except KeyboardInterrupt:
+            break
+        except:
+            print ("No data")
+        if s=='START':
+            print(s)
+            f_out = open(f_out_name, 'a')
+            continue
+        if s=='STOP':
+            print(s)
+            f_out.close()
+            continue
+        try:
+            valuse=s.split(':')
+            f_out.write(valuse[1].strip()+';'+valuse[3].strip()+';'+valuse[5].strip()+';'+valuse[7].strip()+'\n')
+            print(valuse[5].strip()+"   "+str(int(valuse[3].strip())//60000)+":"+str(int(valuse[3].strip())//1000%60))
+        except:
+            print ("Can't decode")
+    f_out.close()
+    ser.close()
+
+def main():
+    save_to_file()
+
+if __name__ == '__main__':
+    main()
